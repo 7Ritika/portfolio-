e9ead8b1803b8aa1f815d07c3a9aa4b038abc782
7Ritika <ritika7@ualberta.ca>
2024-01-19 15:45:14 -0700

    some improvements

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
-function toggleMenu(){
-    const menu=document.querySelector(".menu-links");
-    const icon=document.querySelector(".hamburger-icon");
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-
-    
-    
-}
-function navigateToContact() {
-    // Reset the hash
-    window.location.hash = '';
-    // Set the hash to #contact
-    window.location.hash = 'contact';
-}
-document.addEventListener('DOMContentLoaded', (event) => {
-    // List of image sources
-    const projectImages1 = ["path/to/your/image1.jpg", "path/to/your/image2.jpg", "path/to/another/image3.jpg"];
-    const projectImages2 = [...]; // Similarly for other projects
-    const projectImages3 = [...];
-
-    let currentImageIndex1 = 0;
-    let currentImageIndex2 = 0;
-    let currentImageIndex3 = 0;
-
-    setInterval(() => {
-        currentImageIndex1 = (currentImageIndex1 + 1) % projectImages1.length;
-        document.getElementById('project-img-1').src = projectImages1[currentImageIndex1];
-    }, 5000); // Change every 5000 milliseconds (5 seconds)
-
-    // Repeat for other images
-});
-
+  }
+  
